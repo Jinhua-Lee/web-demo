6116aec19db05ca92440ab74c4c4ce0bee971a03
Jinhua-Lee <Jinhua-Lee@outlook.com>
2022-08-19 10:29:39 +0800

    [init] 初始化提交.

---
+++ src/main/java/com/example/webdemo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.webdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Todo
+ *
+ * @author Jinhua-Lee
+ * @version 1.0
+ * @date 2022/2/11 22:47
+ */
+@RestController
+@RequestMapping(value = "demo")
+public class DemoController {
+
+    @GetMapping(value = "hello")
+    public String hello() {
+        return "hello";
+    }
+}
