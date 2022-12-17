dc002debdcd16364721285b5fd9995f00fc986fb
Jinhua-Lee <Jinhua-Lee@outlook.com>
2022-12-17 21:02:56 +0800

    [feat] Controller访问web静态资源的调试.

--- src/main/java/com/example/webdemo/controller/DemoRestController.java
+++ src/main/java/com/example/webdemo/controller/DemoRestController.java
@@ -1,22 +1,27 @@
 package com.example.webdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Todo
+ * 测试RestController
  *
  * @author Jinhua-Lee
  * @version 1.0
  * @date 2022/2/11 22:47
  */
 @RestController
-@RequestMapping(value = "demo-rest")
+@RequestMapping(value = "demo")
+@Slf4j
 public class DemoRestController {
 
     @GetMapping(value = "hello")
     public String hello() {
+        if (log.isInfoEnabled()) {
+            log.info("hello request accepted.");
+        }
         return "hello";
     }
 }
