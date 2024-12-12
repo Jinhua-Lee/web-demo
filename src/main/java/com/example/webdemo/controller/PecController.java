17231cacf9a2349386430f2cd96ca3e79f0c2dba
Jinhua-Lee <lijinhua@cet-electric.com>
2024-12-12 15:09:13 +0800

    [feat] 多个服务的路由，调试通过.

--- src/main/java/com/example/webdemo/controller/PecController.java
+++ src/main/java/com/example/webdemo/controller/PecController.java
@@ -1,28 +1,40 @@
 package com.example.webdemo.controller;
 
-import com.cet.electric.matterhorn.devicedataservice.api.TestPecInitRestApi;
+import com.cet.electric.matterhorn.devicedataservice.api.TestRestApi;
 import com.cet.electric.matterhorn.devicedataservice.common.PecInitResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Jinhua-Lee
  */
 @RestController
-@RequestMapping(value = "/pec-service")
+@RequestMapping(value = "/pec-core")
 public class PecController {
 
-    private TestPecInitRestApi testPecInitRestApi;
+    private TestRestApi testRestApi;
 
     @GetMapping(value = "init")
     public PecInitResult init() {
-        return testPecInitRestApi.init();
+        return testRestApi.init();
+    }
+
+    @GetMapping(value = "nextIds")
+    public List<Long> nextNodeIds(
+            @RequestParam(value = "nodeType") Long nodeType,
+            @RequestParam(value = "count") Integer count,
+            @RequestParam(value = "tenantId", required = false) Integer tenantId
+    ) {
+        return testRestApi.getNextNodeIds(nodeType, count, tenantId).getData();
     }
 
     @Autowired
-    public void setTestPecInitRestApi(TestPecInitRestApi testPecInitRestApi) {
-        this.testPecInitRestApi = testPecInitRestApi;
+    public void setTestRestApi(TestRestApi testRestApi) {
+        this.testRestApi = testRestApi;
     }
 }
