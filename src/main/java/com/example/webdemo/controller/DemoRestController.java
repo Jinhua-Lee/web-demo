2a22572a0f2dab88f6baec1cd9fbaab2390a5222
Jinhua-Lee <lijinhua@cet-electric.com>
2025-02-27 17:29:23 +0800

    [0.0.3] [feat] 调试服务访问时断网.

--- src/main/java/com/example/webdemo/controller/DemoRestController.java
+++ src/main/java/com/example/webdemo/controller/DemoRestController.java
@@ -3,7 +3,10 @@ package com.example.webdemo.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 测试RestController
@@ -19,9 +22,15 @@ public class DemoRestController {
 
     @GetMapping(value = "/hello")
     public String hello() {
-        if (log.isInfoEnabled()) {
-            log.info("hello request accepted.");
-        }
+        log.info("hello request accepted.");
         return "hello";
+    }
+
+    @GetMapping(value = "/hangup")
+    public String hangup(@RequestParam("hangUpSeconds") Integer hangUpSeconds) throws InterruptedException {
+        log.info("start to hang up.");
+        TimeUnit.SECONDS.sleep(hangUpSeconds);
+        log.info("hang up finished for {} seconds", hangUpSeconds);
+        return "hangup";
     }
 }
