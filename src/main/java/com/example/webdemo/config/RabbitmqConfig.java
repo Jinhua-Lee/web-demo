5fa9e71ef2406b0214121fbff6f4044c6c466b5a
Jinhua-Lee <lijinhua@cet-electric.com>
2025-08-04 18:58:19 +0800

    [chore] 去掉不必要的注释.

--- src/main/java/com/example/webdemo/config/RabbitmqConfig.java
+++ src/main/java/com/example/webdemo/config/RabbitmqConfig.java
@@ -2,10 +2,6 @@ package com.example.webdemo.config;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
