ee47cee8a0ff0df80cdaae8d670c8dd281599eff
Jinhua-Lee <lijinhua@cet-electric.com>
2025-08-05 14:22:38 +0800

    [feat] 调试PecService定时记录接口的Double类型序列化问题.

--- src/main/java/com/example/webdemo/WebDemoApplication.java
+++ src/main/java/com/example/webdemo/WebDemoApplication.java
@@ -2,11 +2,13 @@ package com.example.webdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author Jinhua
  */
 @SpringBootApplication
+@EnableFeignClients
 public class WebDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/webdemo/controller/PecController.java
+++ src/main/java/com/example/webdemo/controller/PecController.java
@@ -5,15 +5,20 @@ import com.cet.electric.matterhorn.devicedataservice.v2.api.DataServiceV2RestApi
 import com.cet.electric.matterhorn.devicedataservice.v2.api.TestRestApi;
 import com.cet.electric.matterhorn.devicedataservice.v2.common.PecInitResult;
 import com.cet.electric.matterhorn.devicedataservice.v2.common.entity.datalog.DatalogGroupData;
+import com.cet.electric.matterhorn.devicedataservice.v2.common.entity.datalog.DatalogValue;
 import com.cet.electric.matterhorn.devicedataservice.v2.common.entity.datalog.MeterDataLogQueryParam;
+import com.example.webdemo.feign.FusionFacadeFeignClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Jinhua-Lee
  */
+@Slf4j
 @RestController
 @RequestMapping(value = "/pec-core")
 public class PecController {
@@ -21,6 +26,8 @@ public class PecController {
     private TestRestApi testRestApi;
 
     private DataServiceV2RestApi dataServiceV2RestApi;
+
+    private FusionFacadeFeignClient fusionFacadeFeignClient;
 
     @GetMapping(value = "init")
     public PecInitResult init() {
@@ -41,7 +48,36 @@ public class PecController {
             @RequestBody MeterDataLogQueryParam param,
             @RequestParam (value = "fill", required = false) Boolean fill,
             @RequestParam (value = "p35", required = false) Boolean p35) {
-        return dataServiceV2RestApi.queryMultiMeterDataLogGroupDataByStartTimeAndInterval(param, fill, p35);
+        ApiResult<List<DatalogGroupData>> pecServiceResult = dataServiceV2RestApi.queryMultiMeterDataLogGroupDataByStartTimeAndInterval(param, fill, p35);
+        ApiResult<List<DatalogGroupData>> facadeResult = fusionFacadeFeignClient.queryMultiMeterDataLogGroupDataByStartTimeAndInterval(param, fill, p35);
+        if (pecServiceResult.isSuccess() && facadeResult.isSuccess()) {
+            // 比较Double值结果是否一致
+            // 1. 先找到deviceId, logicalId, dataTypeId, dataId相等的第一条
+            for (DatalogGroupData pecServiceData : pecServiceResult.getData()) {
+                for (DatalogGroupData facadeData : facadeResult.getData()) {
+                    if (Objects.equals(pecServiceData, facadeData)) {
+                        // 2. 找到第一条时间相等的记录并比较是否相等
+                        findFirstTimeEqualValue(pecServiceData, facadeData);
+                        break;
+                    }
+                }
+            }
+        }
+        return pecServiceResult;
+    }
+
+    private void findFirstTimeEqualValue(DatalogGroupData pecServiceData, DatalogGroupData facadeData) {
+        for (DatalogValue pecServiceValue : pecServiceData.getDataList()) {
+            for (DatalogValue facadeValue : facadeData.getDataList()) {
+                // 3. 找到时间相等的第一条值并比较是否相等
+                if (Objects.equals(pecServiceValue.getTime(), facadeValue.getTime())) {
+                    log.info("pecServiceValue: {}", pecServiceValue);
+                    log.info("facadeValue: {}", facadeValue);
+                    log.info("value equals: {}", Objects.equals(pecServiceValue.getValue(), facadeValue.getValue()));
+                    break;
+                }
+            }
+        }
     }
 
 
@@ -53,5 +89,10 @@ public class PecController {
     @Autowired
     public void setDataServiceV2RestApi(DataServiceV2RestApi dataServiceV2RestApi) {
         this.dataServiceV2RestApi = dataServiceV2RestApi;
+    }
+
+    @Autowired
+    public void setFusionFacadeFeignClients(FusionFacadeFeignClient fusionFacadeFeignClient) {
+        this.fusionFacadeFeignClient = fusionFacadeFeignClient;
     }
 }
