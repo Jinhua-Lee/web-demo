db8f10cf2aa1ccd88bcf5080ae4ce570aa3be46a
Jinhua-Lee <lijinhua@cet-electric.com>
2025-08-05 19:30:36 +0800

    [feat] 调试PecService定时记录接口的Double类型序列化问题.

--- src/main/java/com/example/webdemo/WebDemoApplication.java
+++ src/main/java/com/example/webdemo/WebDemoApplication.java
@@ -2,13 +2,11 @@ package com.example.webdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author Jinhua
  */
 @SpringBootApplication
-@EnableFeignClients
 public class WebDemoApplication {
 
     public static void main(String[] args) {
